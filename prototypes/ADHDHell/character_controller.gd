228caccbca8780b730f7d0c23dd0fbb1d32a8eb5
Lily <lily@sunsave.energy>
2026-07-23 22:18:32 +0100

    Clamp global position to viewport
    Co-authored-by: Aminata <71596982+natleahh@users.noreply.github.com>

--- prototypes/ADHDHell/character_controller.gd
+++ prototypes/ADHDHell/character_controller.gd
@@ -4,3 +4,5 @@ var direction: Vector2
 
 func _process(delta: float) -> void:
 	global_position = lerp(global_position, get_global_mouse_position(), 0.05)
+	var viewportRect: Rect2 = get_viewport_rect()
+	global_position = global_position.clamp(viewportRect.position, viewportRect.position + viewportRect.size)
