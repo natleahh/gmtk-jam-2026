8f89ded4e3f10dbef65b75bf3c64578ac8c04870
Lily <1368025+Lily418@users.noreply.github.com>
2026-07-25 15:42:12 +0100

    Updated boss script with new states

--- prototypes/ADHDHell/boss.gd
+++ prototypes/ADHDHell/boss.gd
@@ -7,35 +7,49 @@ extends Node2D
 @export var patrol_timer: Timer
 
 var velocity: float
+var current_state = BossState.PATROL
+
+enum BossState {PATROL, SUS, CHECKING, PISSED, SATISFIED}
 
 func _ready() -> void:
 	velocity = initial_velocity
-	boss_patrolling_sprite.animation = "walk"
 	set_random_wait_time()
 	
-	for i in 100:
-		print(randfn(5, 2.0) + 1)
+	#for i in 100:
+		#print(randfn(5, 2.0) + 1)
 
 func _process(delta: float) -> void:
-	# Progressing Logic
-	var next_position: float = patrol_follow.progress_ratio + velocity
-	if next_position < 0 or next_position > 1:
-		velocity *= -1
-		
-	patrol_follow.progress_ratio += velocity * delta
-	
-	# Sprite Logic
-	boss_patrolling_sprite.flip_h = velocity > 0
+	match current_state:
+		BossState.PATROL:
+			boss_patrolling_sprite.animation = "patrol"
+			# Progressing Logic
+			var next_position: float = patrol_follow.progress_ratio + velocity
+			if next_position < 0 or next_position > 1:
+				velocity *= -1
+				
+			patrol_follow.progress_ratio += velocity * delta
+			
+			# Sprite Logic
+			boss_patrolling_sprite.flip_h = velocity > 0
+		BossState.SUS:
+			velocity = 0
+			boss_patrolling_sprite.animation = "sus"
+		BossState.CHECKING:
+			# TODO: Check if player is behaving
+			pass
+		BossState.PISSED:
+			boss_patrolling_sprite.animation = "pissed"
+		BossState.SATISFIED:
+			boss_patrolling_sprite.animation = "satisfied"
 
 func _on_timer_timeout() -> void:
-	print("timer")
-	boss_patrolling_sprite.play("glare")
-	velocity = 0
-
+	current_state = BossState.SUS
 
 func _on_boss_patrolling_sprite_animation_finished() -> void:
-	if boss_patrolling_sprite.animation == "glare":
-		boss_patrolling_sprite.animation = "walk"
+	if boss_patrolling_sprite.animation == "sus":
+		current_state = BossState.CHECKING
+	if boss_patrolling_sprite.animation == "pissed" || boss_patrolling_sprite.animation == "satisfied":
+		current_state = BossState.PATROL
 		velocity = initial_velocity * (1 if randi_range(0, 1) == 1 else -1)
 		set_random_wait_time()
 
