870e2a936e755a6976b0678554937cb53a7d2ca3
Aminata Tengbe <71596982+natleahh@users.noreply.github.com>
2026-07-26 12:14:51 +0100

    Unify game over states
    Co-authored-by: Lily <1368025+Lily418@users.noreply.github.com>

--- prototypes/ADHDHell/hud.gd
+++ prototypes/ADHDHell/hud.gd
@@ -11,25 +11,29 @@ class_name HUD extends CanvasLayer
 @export var color_rect_2: ColorRect
 @export var color_rect_3: ColorRect
 
-
 @onready var tasks: Array[ColorRect] = [
 		color_rect_1,
 		color_rect_2,
 		color_rect_3,
 ]
+@export var deadline_alert: Label
+@export var audio_stream_player: AudioStreamPlayer
+
 
 func _ready() -> void:
 	dopamine_system.level_updated.connect(_on_updated_dopamine)
 	OkrSystem.completion_update.connect(_on_update_okr)
 
-		
-	
 func _on_updated_dopamine():
 	progress_bar.value = dopamine_system.level
 
 func _on_update_okr():
 	tasks[OkrSystem.current].color = Color("green")
-		
-func game_over():
+
+func game_over(text: String, sound: bool):
+	deadline_alert.text = text
+	deadline_alert.visible = true
 	game_over_screen.visible = true
-	
+	visible = true
+	if sound:
+		audio_stream_player.play()
--- prototypes/ADHDHell/main.gd
+++ prototypes/ADHDHell/main.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
-
-
 @export var dopamine_system: Dopaminesystem
 @export var hud: HUD
 @export var clock: Sprite2D
 @export var okr_timer: Timer
-
+@export var deadline_alert: Label
 
 func _ready() -> void:
 	dopamine_system.level_depleted.connect(game_over)
@@ -15,6 +13,12 @@ func _process(delta: float) -> void:
 	clock.update_clock(okr_timer.time_left, okr_timer.wait_time)
 
 func game_over():
+	var game_over_text: String
+	if dopamine_system.level <= 0:
+		game_over_text = "NO MORE DOPAMINE, SORRY! :("
+	else:
+		game_over_text = "GAME OVER: OH NO, WE MISSED THE EOQ (End of Quarter) DEALINE :("
+	hud.game_over(game_over_text, dopamine_system.level > 0)
 	process_mode = Node.PROCESS_MODE_DISABLED
-	hud.game_over()
+	
 	
